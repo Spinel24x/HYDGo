2a095459c029e3c5c79780e360c5b8ded2b8f244
Spinel24x <spinelapper@gmail.com>
2026-08-08 01:47:30 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "========================================="
-echo "  DNSCrypt Proxy - Starting"
+echo "  DNSCrypt Proxy  Starting"
 echo "========================================="
 echo ""
 
