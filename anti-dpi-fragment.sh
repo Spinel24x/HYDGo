01bff4f4628374f65921ae48e7f01a86b98195e5
Spinel24x <spinelapper@gmail.com>
2026-08-08 01:25:35 +0330

    Delete anti-dpi-fragment.sh

--- anti-dpi-fragment.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-if [ "${ENABLE_FRAGMENT:-false}" = "true" ]; then
-    echo "  [Module] Fragment: Active"
-    iptables -t mangle -A OUTPUT -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --set-mss 1200 2>/dev/null || true
-fi
